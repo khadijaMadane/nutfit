c692b38ddc2aa131f443a9c3e366c5bd953a2ee9
khadija <khadijabenmadane2106@gmail.com>
2023-05-10 20:46:19 +0200

    nothing special

--- app/src/main/java/com/example/nutfit/DashboardMainActivity.kt
+++ app/src/main/java/com/example/nutfit/DashboardMainActivity.kt
@@ -10,7 +10,11 @@ class DashboardMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_main)
-
+        val nutrientCount = intent.getIntExtra("nutrientCount", 0)
+        val nutrientCountTextView = findViewById<TextView>(R.id.aaaa).apply {
+            text= nutrientCount.toString()
+        }
+        //nutrientCountTextView.text = nutrientCount.toString()
 
     }
 }
